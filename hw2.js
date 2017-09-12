68d2b883fc02876d16d84e5f1f07c0ffcc26548f
Marina Sargsyan <marina.sargsyan98@gmail.com>
2017-09-12 16:02:26 +0400

    hw2 commited

---
+++ hw2.js
@@ -0,0 +1,8 @@
+const weightNewborn = 50/1000;
+const ageInDays = 53;
+const weightNow = ageInDays * 20/1000 + weightNewborn
+weightNow >= 0.8 ? console.log('The fox has already grown') : console.log('The fox will get bigger');
+
+
+
+
